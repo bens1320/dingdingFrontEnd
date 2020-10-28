bc8d30f2679426f066061caa403d2ff4acadb7f8
bens1320 <yu1piao09@163.com>
2020-10-29 00:29:10 +0800

    修改后端地址

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 yarn build
-docker build -t registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.3 .
-docker push registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.3
+docker build -t registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.4 .
+docker push registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.4
 
 # 服务器
 docker stop dingding
 docker rm dingding
-docker pull registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.3
-docker run -d -p 8003:80 --name dingding registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.3
+docker pull registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.4
+docker run -d -p 8003:80 --name dingding registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.4
