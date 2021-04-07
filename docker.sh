0aee68bb511fc1b03b5706e6c0fb171b558a9359
bens1320 <yu1piao09@163.com>
2021-04-07 22:15:56 +0800

    修改 footer 的高度

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 yarn build
-docker build -t registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.4 .
-docker push registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.4
+docker build -t registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.6 .
+docker push registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.6
 
 # 服务器
 docker stop dingding
 docker rm dingding
-docker pull registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.4
-docker run -d -p 8003:80 --name dingding registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.4
+docker pull registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.6
+docker run -d -p 8003:80 --name dingding registry.cn-shanghai.aliyuncs.com/hfrost/dingding:1.6
